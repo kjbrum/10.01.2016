5916cade203d94eab425a93076481171f1b5524f
Kyle Brumm <kjbrum@msn.com>
2016-02-22 22:47:03 -0600

    Add new fonts and minor section styling

--- js/src/main.js
+++ js/src/main.js
@@ -5,9 +5,9 @@
             var $this = $(this);
             var format = '<div class="g g-s-2 g-m-4"><div class="gi days"><div class="block">%D<span>days</span></div></div><div class="gi hours"><div class="block">%H<span>hours</span></div></div><div class="gi minutes"><div class="block">%M<span>minutes</span></div></div><div class="gi seconds"><div class="block">%S<span>seconds</span></div></div></div>';
             if (event.elapsed) {
-                $this.html(event.strftime('<h3 class="heading">Happily Married</h3>'+format));
+                $this.html(event.strftime('<h3 class="heading">Happily <span>Married</span></h3>'+format));
             } else {
-                $this.html(event.strftime('<h3 class="heading">Time to Wait</h3>'+format));
+                $this.html(event.strftime('<h3 class="heading">Time to <span>Wait</span></h3>'+format));
             }
         });
 
