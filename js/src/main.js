361d931062548f7d5a257488b3c89f01ab8f2cd7
Kyle Brumm <kjbrum@msn.com>
2016-02-23 11:02:47 -0600

    Update the countdown design

--- js/src/main.js
+++ js/src/main.js
@@ -3,7 +3,7 @@
     $('.countdown').countdown('2016/10/01', { elapse: true })
         .on('update.countdown', function(event) {
             var $this = $(this);
-            var format = '<div class="g g-s-2 g-m-4"><div class="gi days"><div class="block">%D<span>days</span></div></div><div class="gi hours"><div class="block">%H<span>hours</span></div></div><div class="gi minutes"><div class="block">%M<span>minutes</span></div></div><div class="gi seconds"><div class="block">%S<span>seconds</span></div></div></div>';
+            var format = '<div class="g g-s-2 g-m-4"><div class="gi days"><div class="block"><span class="number">%D</span><span class="suffix">days</span></div></div><div class="gi hours"><div class="block"><span class="number">%H</span><span class="suffix">hours</span></div></div><div class="gi minutes"><div class="block"><span class="number">%M</span><span class="suffix">minutes</span></div></div><div class="gi seconds"><div class="block"><span class="number">%S</span><span class="suffix">seconds</span></div></div></div>';
             if (event.elapsed) {
                 $this.html(event.strftime('<h3 class="heading">Happily <span>Married</span></h3>'+format));
             } else {
